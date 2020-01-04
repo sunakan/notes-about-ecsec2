0753024a220a6e111a472864a3cb9a88781c1af6
sunakan <sunakan03@gmail.com>
2020-01-04 15:20:33 +0900

    Add shared-infra's lb_listener_arn for app-infra
    listener rule require lb_listener_arn

--- 02-separated-infra-in-single-account/shared-infra/terraform/outputs.tf
+++ 02-separated-infra-in-single-account/shared-infra/terraform/outputs.tf
@@ -2,6 +2,10 @@ output "vpc_id" {
   value       = aws_vpc.this.id
   description = "VPCのID"
 }
+output "lb_listener_arn" {
+  value       = aws_lb_listener.this.arn
+  description = "ALBのlistenerのarn"
+}
 output "public_subnet_ids" {
   value = [
     aws_subnet.public_1.id,
