0dc2e8b63f52342a856b1025d610f233e3af7f21
sunakan <sunakan03@gmail.com>
2020-01-08 19:28:32 +0900

    app環境変数を全部出すようにする

--- mini-pod/app/app.rb
+++ mini-pod/app/app.rb
@@ -8,6 +8,20 @@ get '/sunatra' do
   'Welcome sunatra!!'
 end
 
+get '/env' do
+  "env => #{ENV['ENV'] || '入ってません'}"
+end
+
+get '/secret' do
+  "secret => #{ENV['SECRET'] || '入ってません'}"
+end
+
+get '/env_all' do
+  all = ""
+  ENV.each {|k,v| all += "#{k} => #{v}\n"}
+  all
+end
+
 get '/health_check' do
   'OK'
 end
